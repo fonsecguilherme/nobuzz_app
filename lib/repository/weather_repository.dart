4a0373858c6683b426e0b047d08fed7f0146db4b
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-10-31 23:57:40 -0300

    Repository class creation

---
+++ lib/repository/weather_repository.dart
@@ -0,0 +1,26 @@
+import 'package:nobuzz_app/core/htpp/http_dio.dart';
+import 'package:nobuzz_app/core/htpp/i_http.dart';
+import 'package:nobuzz_app/model/image_model.dart';
+import 'package:nobuzz_app/model/weather_model.dart';
+
+class WeatherRepository {
+  final IHttp http = HttpDio();
+
+  Future<Weather?> getAllWeatherRepo() async {
+    final response = await http.post('informacoes_do_tempo');
+
+    if (response.data != null) {
+      return Weather.fromJson(response.data);
+    }
+    return null;
+  }
+
+  Future<Image?> getImageRepo() async {
+    final response = await http.post('tempo');
+
+    if (response.data != null) {
+      return Image.fromJson(response.data);
+    }
+    return null;
+  }
+}
