9cf97852b3945f9735288b12459b80fbd18a3e7c
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-10-31 23:54:34 -0300

    Widget componentization

---
+++ lib/view/pages/home_page/widgets/cities_builder_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:nobuzz_app/model/weather_model.dart';
+
+class CitiesBuilder extends StatefulWidget {
+  final Weather result;
+
+  const CitiesBuilder({super.key, required this.result});
+
+  @override
+  State<CitiesBuilder> createState() => _CitiesBuilderState();
+}
+
+class _CitiesBuilderState extends State<CitiesBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    List<Estados> estados = widget.result.result!.estados!;
+
+    return ListView.builder(
+      itemCount: estados.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
+          child: Container(
+            height: 90,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF957DCD),
+                  Color(0xFF523D7F),
+                ],
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 36.0),
+                  child: Text(
+                    estados[index].estado!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 36.0),
+                  child: Image.asset('assets/images/tempestade.png'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
