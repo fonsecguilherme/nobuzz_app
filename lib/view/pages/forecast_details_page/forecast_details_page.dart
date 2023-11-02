d560b8fa4a34ad0329f6cd06f3482c14e7de76aa
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-02 20:43:28 -0300

    Widget refactor

--- lib/view/pages/forecast_details_page/forecast_details_page.dart
+++ lib/view/pages/forecast_details_page/forecast_details_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nobuzz_app/core/cubit/weather_cubit.dart';
+import 'package:nobuzz_app/core/cubit/weather_state.dart';
 import 'package:nobuzz_app/model/weather_model.dart';
+import 'package:nobuzz_app/view/pages/forecast_details_page/widgets/bottom_carousel_widget.dart';
 import 'package:nobuzz_app/view/pages/forecast_details_page/widgets/period_item_widget.dart';
 import 'package:nobuzz_app/view/pages/forecast_details_page/widgets/top_weather_banner.dart';
 
-class ForecastDetailPage extends StatelessWidget {
+class ForecastDetailPage extends StatefulWidget {
   final List<Periodo> periodo;
 
   const ForecastDetailPage({
     super.key,
     required this.periodo,
   });
+
+  @override
+  State<ForecastDetailPage> createState() => _ForecastDetailPageState();
+}
+
+class _ForecastDetailPageState extends State<ForecastDetailPage> {
+  WeatherCubit get weatherCubit => context.read<WeatherCubit>();
+
+  @override
+  void initState() {
+    super.initState();
+    weatherCubit.getAllWeather();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -45,7 +62,7 @@ class ForecastDetailPage extends StatelessWidget {
               Flexible(
                 flex: 3,
                 child: TopWeatherBanner(
-                  periodo: periodo,
+                  periodo: widget.periodo,
                 ),
               ),
               const Text(
@@ -66,24 +83,44 @@ class ForecastDetailPage extends StatelessWidget {
                   fontSize: 18,
                 ),
               ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  color: Colors.blue,
-                ),
-              ),
+              Flexible(flex: 1, child: _bottomStates()),
             ],
           ),
         ),
       );
 
+  Widget _bottomStates() => BlocBuilder<WeatherCubit, WeatherState>(
+        builder: _builder,
+      );
+
+  Widget _builder(BuildContext context, WeatherState state) {
+    if (state is WeatherErrorState) {
+      return Center(
+        child: Text(state.errorMessage),
+      );
+    } else if (state is WeatherLoadingState) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is WeatherInitialState) {
+      return const Center(
+        child: Text('Estado inicial'),
+      );
+    } else if (state is WeatherFetchedState) {
+      return BottomCarouselWidget(
+        weather: state.result,
+      );
+    }
+    return const SizedBox();
+  }
+
   Widget _todayForecast() => SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
           children: [
-            PeriodItemWidget(info: periodo[0].manha, periodo: 'Manhã'),
-            PeriodItemWidget(info: periodo[0].tarde, periodo: 'Tarde'),
-            PeriodItemWidget(info: periodo[0].noite, periodo: 'Noite'),
+            PeriodItemWidget(info: widget.periodo[0].manha, periodo: 'Manhã'),
+            PeriodItemWidget(info: widget.periodo[0].tarde, periodo: 'Tarde'),
+            PeriodItemWidget(info: widget.periodo[0].noite, periodo: 'Noite'),
           ],
         ),
       );
