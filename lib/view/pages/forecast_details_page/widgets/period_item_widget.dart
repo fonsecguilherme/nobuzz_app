57f4a8542cd465167cdf2bba0d7757875e12c64d
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-02 16:22:22 -0300

    Qidget componentization

---
+++ lib/view/pages/forecast_details_page/widgets/period_item_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:nobuzz_app/helpers/functions.dart';
+import 'package:nobuzz_app/model/weather_model.dart';
+
+class PeriodItemWidget extends StatelessWidget {
+  final List<Info>? info;
+  final String periodo;
+
+  const PeriodItemWidget({
+    required this.periodo,
+    super.key,
+    this.info,
+  });
+//TODO: add navigation tofinal screen
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding:
+            const EdgeInsets.only(right: 8.0, left: 8.0, top: 8.0, bottom: 36),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 155,
+          width: 115,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF957DCD),
+                Color(0xFF523D7F),
+              ],
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                periodo,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600),
+              ),
+              Image.asset(
+                Functions.imageWeather(info![0].tempo),
+              ),
+              Text(
+                '${info![0].graus}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
