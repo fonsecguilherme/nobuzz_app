ccf8dc97dd0887bd7ee875ee75088f74d986cbfc
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-02 20:52:35 -0300

    Added navigation

--- lib/view/pages/forecast_details_page/widgets/period_item_widget.dart
+++ lib/view/pages/forecast_details_page/widgets/period_item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nobuzz_app/helpers/functions.dart';
 import 'package:nobuzz_app/model/weather_model.dart';
+import 'package:nobuzz_app/view/pages/feedback_page/feedback_page.dart';
 
 class PeriodItemWidget extends StatelessWidget {
   final List<Info>? info;
@@ -16,46 +17,56 @@ class PeriodItemWidget extends StatelessWidget {
   Widget build(BuildContext context) => Padding(
         padding:
             const EdgeInsets.only(right: 8.0, left: 8.0, top: 8.0, bottom: 36),
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          height: 155,
-          width: 115,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF957DCD),
-                Color(0xFF523D7F),
-              ],
+        child: GestureDetector(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            height: 155,
+            width: 115,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF957DCD),
+                  Color(0xFF523D7F),
+                ],
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
             ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                periodo,
-                style: const TextStyle(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  periodo,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600),
+                ),
+                Image.asset(
+                  Functions.imageWeather(info![0].tempo),
+                ),
+                Text(
+                  '${info![0].graus}',
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 12,
-                    fontWeight: FontWeight.w600),
-              ),
-              Image.asset(
-                Functions.imageWeather(info![0].tempo),
-              ),
-              Text(
-                '${info![0].graus}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const FeedbackPage(),
+              ),
+            );
+          },
         ),
       );
 }
