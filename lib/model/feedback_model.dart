95056b1b703f9990aca2c6bfbeb6ad44040b01be
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-02 22:19:34 -0300

    Created feedback model

---
+++ lib/model/feedback_model.dart
@@ -0,0 +1,33 @@
+class Feedback {
+  Result? result;
+
+  Feedback({this.result});
+
+  Feedback.fromJson(Map<String, dynamic> json) {
+    result = json['result'] != null ? Result.fromJson(json['result']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (result != null) {
+      data['result'] = result!.toJson();
+    }
+    return data;
+  }
+}
+
+class Result {
+  List<String>? estados;
+
+  Result({this.estados});
+
+  Result.fromJson(Map<String, dynamic> json) {
+    estados = json['estados'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['estados'] = estados;
+    return data;
+  }
+}
