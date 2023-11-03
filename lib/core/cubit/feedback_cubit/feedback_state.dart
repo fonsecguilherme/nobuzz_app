ecf1a0a9934701cbf64406ebf9128d08597a6f41
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-11-02 22:19:16 -0300

    Created feedback and cubit files

---
+++ lib/core/cubit/feedback_cubit/feedback_state.dart
@@ -0,0 +1,20 @@
+
+import 'package:nobuzz_app/model/feedback_model.dart';
+
+abstract class FeedbackState {}
+
+class FeedbackInitialState extends FeedbackState {}
+
+class FeedbackLoadingState extends FeedbackState {}
+
+class FeedbackFetchedState extends FeedbackState {
+  final Feedback result;
+
+  FeedbackFetchedState(this.result);
+}
+
+class FeedbackErrorState extends FeedbackState {
+  final String errorMessage;
+
+  FeedbackErrorState(this.errorMessage);
+}
